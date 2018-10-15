5fa4fdec80336f09867c7ce76d3841b9a05f08d9
Yung-Sheng Lu <yungshenglu1994@gmail.com>
2018-10-15 17:13:52 +0800

    Update READEMD.md in ubuntu-env

--- ubuntu-env/main.sh
+++ ubuntu-env/main.sh
@@ -1,63 +1,88 @@
 #!/bin/bash -e
 
 function build {
-    # $1 = image name, $2 = external port
+    # $1: the name of docker image
+    # $2: the external port
     if [ $# -lt 2 ]; then
-        echo "Error Format..."
-        echo "./main.sh build <image_name> <external_port>"
+        echo "[ERROR] The format of command is WRONG"
+        echo "[INFO] ./main.sh build <IMAGE_NAME> <EXTERNAL_PORT>"
         exit
     fi
-    echo "Image name: $1"
-    echo "External port: $2"
-    # build the image from Dockerfile
+
+    echo "[INFO] Docker image: $1"
+    echo "[INFO] External port: $2"
+    # Build the image from Dockerfile
     docker build -f Dockerfile -t $1 .
-    # build the container from this image
+    # Build the container named cn2018_c from the image named cn2018
+    docker run -d -p $2:22 --privileged --name $1"_c" $1 > /dev/null
+    # List port 22 mapping on cn2018_c
+    docker port $1"_c" 22
+}
+
+function run {
+    # $1: the name of docker image
+    # $2: the external port
+    if [ $# -lt 2 ]; then
+        echo "[ERROR] The format of command is WRONG"
+        echo "[INFO] ./main.sh run <CONTAINER_NAME>"
+        exit
+    fi
+
+    echo "[INFO] Run Docker container named $1 on port $2"
+    # build from existed image
     docker run -d -p $2:22 --privileged --name $1"_c" $1 > /dev/null
     # find which port mapping to 22
     docker port $1"_c" 22
 }
 
 function clean {
-    # stop and rm
-    docker container stop ubuntu_c 
-    docker container rm ubuntu_c
-    # rm this image
-    docker image rm ubuntu
+    # $1: the name of docker container
+    if [ $# -lt 1 ]; then
+        echo "[ERROR] The format of command is WRONG"
+        echo "[INFO] ./main.sh clean <CONTAINER_NAME>"
+        exit
+    fi
+
+    echo "[INFO] Stop and remove the container named $1"
+    # Stop and remove container
+    docker container stop $1
+    docker container rm $1
 }
 
-function run {
-    # build from existed image
-    docker run -d -p 3000dos:22 --privileged --name ubuntu_c ubuntu > /dev/null
-    # find which port mapping to 22
-    docker port ubuntu_c 22
+function remove {
+    # $1: the name of docker image
+    if [ $# -lt 1 ]; then
+        echo "[ERROR] The format of command is WRONG"
+        echo "[INFO] ./main.sh remove <IMAGE_NAME>"
+        exit
+    fi
+
+    echo "[INFO] Remove the Docker image named $1"
+    # Remove the docker image
+    docker image rm $1
 }
 
-function cleanall {
-    # stop all container
-    docker stop $(docker ps -a -q)
-    # remove all container
-    docker rm $(docker ps -a -q)
-}
-
-function delall {
-    # delete all docker images (with --force)
-    docker rmi -f $(docker images -q)
-}
-
-# main
-# '$#' refer to the number of parameters (receive at runtime)
-# '$1' denote the first command line argument passed
+# Main
+# $#: the number of parameters (receive at runtime)
+# $1: the first command line argument passed
 
 if [ $# -eq 0 ]; then
-    echo "Usage: $(basename $0) {build|run|clean|cleanall}"
-    echo "\t./main.sh build <image_name> <external_port>"
+    echo "[INFO] Usage: $(basename $0) {build|run|clean|remove}"
+    echo "[INFO] \t./main.sh build <image_name> <external_port>"
     exit
 fi
 
 if [ "$1" == "build" ]; then
-    # $2 = image name
-    # $3 = external port
     $1 $2 $3
+    exit
+elif [ "$1" == "run" ]; then
+    $1 $2
+    exit
+elif [ "$1" == "clean" ]; then
+    $1 $2
+    exit
+elif [ "$1" == "remove" ]; then
+    $1 $2
     exit
 fi
 
